1ef9d8c4f8f1a0866a3e7ca80b7b339d439d1930
SifanGeremu <wins33975@gmail.com>
2026-01-29 16:13:05 +0300

    feat(auth): integrate Spatie roles & permissions with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::post('/register',[AuthController::class,'AuthController@store']);
+Route::post('/login',[AuthController::class,'AuthController@store']);
+Route::post('/logout',[AuthController::class,'AuthController@destroy']);
