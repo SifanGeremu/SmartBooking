27cccc6ea2bdc760053d41ff0cf39855f19951af
SifanGeremu <wins33975@gmail.com>
2026-01-30 22:36:43 +0300

    feat(models): define User-Appointments relationships

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Appointment extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date_time',
+        'status',
+        'notes',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
