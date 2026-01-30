1cfa0006a08fdc43f07a609a7378499fc7cb9adb
SifanGeremu <wins33975@gmail.com>
2026-01-30 16:12:39 +0300

    feat-auth Add login logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
     $request->validate([
         'name'=>'required|string|max:255',
         'email'=>'required|string|email|max:255|unique:users',
-        'password'=>'required|string|min:8|confirmed',
+        'password'=>'required|string|min:8',
     ]);
 
     $user = User::Create([
@@ -31,6 +31,15 @@ class AuthController extends Controller
      
     
     public function Login(Request $request){
+        $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|string',
+        ]);
+
+
+        return response()->json([
+            'message'=>'Login successfull'
+        ],200);
 
     
     }
